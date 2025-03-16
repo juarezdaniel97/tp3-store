895928a659a2cfe1315c5aa8e2dc752b41046a79
Daniel Juarez <juarezz.daniel97@gmail.com>
2025-03-16 11:06:14 -0300

    header terminado

--- src/components/layout/Desktop.jsx
+++ src/components/layout/Desktop.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import {ShoppingCart} from "lucide-react";
 import MenuItem from '../shared/MenuItem';
 
 const Desktop = ({menuItems}) => {
@@ -16,14 +15,6 @@ const Desktop = ({menuItems}) => {
                             />
                         ))
                     }
-
-                     {/* Carrito */}
-                    <button className="relative p-2">
-                        <ShoppingCart size={24} className="text-gray-700  dark:text-white" />
-                        <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs px-2 rounded-full">
-                            3
-                        </span>
-                    </button>
                 </div>
     )
 }
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -14,36 +14,64 @@ const {theme, toggleTheme} = useThemeContext();
         <nav className='bg-slate-100 text-black dark:bg-gray-900 shadow-md p-4'>
             <div className='max-w-7xl mx-auto flex items-center justify-between'>
                 {/* Logo */}
-                <h1 className='text-2xl font-bold text-gray-800  dark:text-white'>Stored</h1>
-            
+                {/* <h1 className='text-2xl font-bold text-gray-800  dark:text-white'>Stored</h1> */}
+                <div>
+                    <div className="flex items-center space-x-2">
+                        <div className="w-3 h-6 bg-gray-600 dark:bg-white rounded-sm" />
+                        <div className="w-1 h-6 bg-indigo-600 rounded-sm" />
+                        <span className="text-dark dark:text-white text-xl font-semibold">Store</span>
+                    </div>
+                </div>
                 {/* Desktop */}
                     <Desktop
                         menuItems={menuItems}
                     />
 
-                    {/* Carrito */}
-                        <button className="md:hidden relative p-2">
+                    {/* Card and Theme for Desktop */}
+                    <div className='hidden md:block space-x-10'>
+                        
+                        {/* Carrito */}
+                        <button className="relative p-2">
                             <ShoppingCart size={24} className="text-gray-700 dark:text-white" />
                             <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs px-2 rounded-full">
                             3
                             </span>
                         </button>
 
-                        {/* Mobile */}
-                        <div>
-                             {/* Button Theme */}
-                                <Button
-                                    style={'p-2 rounded-lg bg-gray-200 dark:bg-gray-800 cursor-pointer'}
+                        {/* Button Theme */}
+                        <Button
+                            style={'p-2 rounded-lg bg-gray-200 dark:bg-gray-800 cursor-pointer'}
+                            action={toggleTheme}
+                            name={theme === "dark" ? <Sun size={20} className="text-yellow-500" /> : <Moon size={20} className="text-blue-900" />}
+                        /> 
+                    </div>
+
+
+                    {/*Card and Theme for Mobile */}
+                    <div className='md:hidden space-x-4'>
+
+                        {/* Carrito */}
+                        <button className="relative p-2">
+                            <ShoppingCart size={24} className="text-gray-700 dark:text-white" />
+                            <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs px-2 rounded-full">
+                                3
+                            </span>
+                        </button>
+
+                        {/* Button Theme */}
+                        <Button
+                            style={'p-2 rounded-lg bg-gray-200 dark:bg-gray-800 cursor-pointer'}
                                     action={toggleTheme}
-                                    name={theme === "dark" ? <Sun size={20} className="text-yellow-500" /> : <Moon size={20} className="text-blue-900" />}
-                                /> 
-                            {/* Menú Burger */}
-                                <Button
-                                    style={'md:hidden p-2 cursor-pointer'}
+                            name={theme === "dark" ? <Sun size={20} className="text-yellow-500" /> : <Moon size={20} className="text-blue-900" />}
+                        /> 
+                                
+                        {/* Menú Burger */}
+                        <Button
+                        style={'p-2 cursor-pointer'}
                                     action={setIsOpenMenu}
-                                    name={<Menu size={28} className="text-gray-700 dark:text-white" />}
-                                />
-                        </div>
+                            name={<Menu size={28} className="text-gray-700 dark:text-white" />}
+                        />
+                    </div>
             </div>
 
             {/* Mobile */}
