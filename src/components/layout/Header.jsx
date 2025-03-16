50f719087b7677b02b00c6993f02edeefcafc535
Daniel Juarez <juarezz.daniel97@gmail.com>
2025-03-16 20:28:46 -0300

    mostrando la cantidad de productos que tiene el carrito en el ícono de mobile

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -61,9 +61,13 @@ const { getCountPrducts } = useCarritoContext()
                         {/* Carrito */}
                         <button className="relative p-2">
                             <ShoppingCart size={24} className="text-gray-700 dark:text-white" />
-                            <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs px-2 rounded-full">
-                                3
-                            </span>
+                            {
+                                getCountPrducts()>0 && (
+                                    <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs px-2 rounded-full">
+                                        {getCountPrducts()}
+                                    </span>
+                                )
+                            }
                         </button>
 
                         {/* Button Theme */}
